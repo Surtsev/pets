93369cc9bd40190d5df8677818d12477d56bbb69
Surtsev <surtsev2006@gmail.com>
2026-04-14 21:12:52 +0300

    before hobbing

---
+++ python/xls-parser/main.py
@@ -0,0 +1,10 @@
+import os
+
+def main(file_path: str):
+   if !os.path.exists(file_path):
+    print(f"{file_path} does not exist or incorrect path. Returning...")
+    return 
+    
+
+if __name__ == "__main__":
+    main(file_path: str)
