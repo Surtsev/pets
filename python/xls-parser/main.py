a9def613cf9d0d46b29b2c0925df1a25fcccecab
Surtsev <surtsev2006@gmail.com>
2026-04-26 18:20:50 +0300

    printing all rows in table

--- python/xls-parser/main.py
+++ python/xls-parser/main.py
@@ -1,10 +1,25 @@
+import sys
 import os
+import pandas as pd
 
 def main(file_path: str):
-   if !os.path.exists(file_path):
-    print(f"{file_path} does not exist or incorrect path. Returning...")
-    return 
-    
+    if not os.path.exists(file_path):
+        print(f"{file_path} does not exist or incorrect path. Returning...")
+        return 
+    df = pd.read_excel(file_path)
+
+    df_str = df.astype(str)
+
+    result = df_str.values.tolist()
+    for i,row in enumerate(result):
+        print(f"{i}: {row}")
+
+
 
 if __name__ == "__main__":
-    main(file_path: str)
+    if len(sys.argv) > 1:
+        file_path = sys.argv[1]
+        main(file_path)
+    else:
+        print("Пожалуйста, укажите путь к файлу Excel")
+        print("Пример: python main.py '/путь/к/файлу.xlsx'")
