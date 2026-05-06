ec723bfcf0441de9794107fee65b4a8111bfd8c4
Surtsev <surtsev2006@gmail.com>
2026-05-06 22:59:18 +0300

    add days enum

--- python/xls-parser/main.py
+++ python/xls-parser/main.py
@@ -1,19 +1,34 @@
 import sys
 import os
 import pandas as pd
+import datetime as dt
+from enum import Enum
+
+
+class Days(Enum):
+    Monday = 0
+    Tuesday = 1
+    Wednesday = 2
+    Thursday = 3
+    Friday = 4
+    Saturday = 5
+    Sunday = 6
+
 
 def main(file_path: str):
     if not os.path.exists(file_path):
         print(f"{file_path} does not exist or incorrect path. Returning...")
-        return 
+        return
     df = pd.read_excel(file_path)
 
     df_str = df.astype(str)
 
     result = df_str.values.tolist()
-    for i,row in enumerate(result):
+    now = dt.datetime.now()
+    print(now.weekday())
+    print(now)
+    for i, row in enumerate(result):
         print(f"{i}: {row}")
-
 
 
 if __name__ == "__main__":
